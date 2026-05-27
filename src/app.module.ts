bdce5717d0155bd7399e77e7a078a3993a9c8bc0
Jaume Sabater <jsabater@gmail.com>
2026-05-27 16:30:57 +0200

    Import resources module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ResourcesModule } from './resources/resources.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ResourcesModule],
   controllers: [AppController],
   providers: [AppService],
 })
