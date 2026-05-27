bcca6f55b166ba0f066d3c930d7812634bc1a83d
Jaume Sabater <jsabater@gmail.com>
2026-05-27 18:08:39 +0200

    Add SQLite database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ResourcesModule } from './resources/resources.module';
 
 @Module({
-  imports: [UsersModule, ResourcesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'database.sqlite',
+      synchronize: true,
+      autoLoadEntities: true,
+      // entities: ['dist/**/*.entity.js'],
+    }),
+    UsersModule,
+    ResourcesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
